e9862c727e6fb44afe6ad395703ad40e64645fe2
bhoomtawathplinsut <bhoomtawath.plinsut@kreditech.com>
2019-05-05 15:52:28 +0700

    unary pingpong

---
+++ unarypingpong/client/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	pb "github.com/varshard/helloproto/unarypingpong/pingpong"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func SendPing(client pb.PingPongClient) (*pb.Pong, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	ping := pb.Ping{
+		Id:      1,
+		Message: "Ping",
+	}
+	pong, err := client.UnaryPing(ctx, &ping)
+	statusCode := status.Code(err)
+	if statusCode != codes.OK {
+		return nil, err
+	}
+	fmt.Printf("Pong: %d, statusCode: %s\n", pong.Id, statusCode.String())
+	return pong, err
+}
+
+func StartPingPongClient() {
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	conn, err := grpc.Dial("127.0.0.1:9000", opts...)
+	if err != nil {
+		panic(err)
+	}
+
+	client := pb.NewPingPongClient(conn)
+
+	_, err = SendPing(client)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Finish Pinging")
+}
+
+func main() {
+	StartPingPongClient()
+}
---
+++ unarypingpong/server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"google.golang.org/grpc"
+
+	pb "github.com/varshard/helloproto/unarypingpong/pingpong"
+)
+
+type PingPongServerImpl struct {
+}
+
+func (s *PingPongServerImpl) UnaryPing(ctx context.Context, ping *pb.Ping) (*pb.Pong, error) {
+	fmt.Println("Ping Received")
+
+	resp := pb.Pong{
+		Id:      ping.Id,
+		Message: ping.Message,
+	}
+
+	return &resp, nil
+}
+
+func StartPingPongServer() {
+	server := PingPongServerImpl{}
+
+	lis, err := net.Listen("tcp", "localhost:9000")
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterPingPongServer(grpcServer, &server)
+
+	if err = grpcServer.Serve(lis); err != nil {
+		panic(err)
+	}
+	fmt.Println("pingpong started")
+}
+
+func main() {
+	StartPingPongServer()
+}
